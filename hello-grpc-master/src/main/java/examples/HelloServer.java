5411272cf412633fb8c1f9e33bcdc90878500d82
grzesiakm <mrtgrzesiak@gmail.com>
2021-11-04 22:08:27 +0100

    simple greeting - example grpc

---
+++ hello-grpc-master/src/main/java/examples/HelloServer.java
@@ -0,0 +1,57 @@
+package examples;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import services.GreeterImpl;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class HelloServer {
+
+    private static final Logger logger = Logger.getLogger(HelloServer.class.getName());
+
+    private Server server;
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            try {
+                HelloServer.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloServer server = new HelloServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+}
---
+++ hello-grpc-master/src/main/java/services/GreeterImpl.java
@@ -0,0 +1,16 @@
+package services;
+
+import generated.GreeterGrpc;
+import generated.HelloReply;
+import generated.HelloRequest;
+import io.grpc.stub.StreamObserver;
+
+public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
